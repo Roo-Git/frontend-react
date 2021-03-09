93d411528aa53df2c9df17b7b317eca6d9923998
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-03-09 20:30:48 +0100

    Add button to navbar

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// import Button from '../../components/Button/Button';
+import Button from '../../components/Button/Button';
 import './Home.css'
 
 const Home = (props) => {
 
    
-//<Button nombre='Register' destino='register'/>
+
     return (
         <div className="masterHome">
             <div className="header">
@@ -13,7 +13,10 @@ const Home = (props) => {
                 <div className="logo">Logo Empresa</div>
                 <div className="titulo">Título Empresa</div>
                 </div>
-                <div className="navbar">Navbar</div>
+                <div className="navbar">Navbar
+                    <Button name='Register' destiny='register'/>
+                    <Button name='Login' destiny='login'/>
+                </div>
                 <div className="containerPhone">
                     <div className="logoPhone">Logo</div>
                     <div className="phoneNumber">Phone Number</div>
